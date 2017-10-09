5c8089e68c045ccc91119006e798fca8653507b8
Daadler6 <dadler@madmobile.com>
2017-10-09 15:14:20 -0400

    working resolver

--- src/models/coordinates.js
+++ src/models/coordinates.js
@@ -4,14 +4,20 @@ const { formatAddress } = require('../helpers');
  * Gets GPS Coordinates from Google Maps API
  */
 async function getCoordinates(locationInfo, googleMapsConnector) {
+	let longitude, latitude;
 	const formattedAddress = formatAddress(locationInfo);
-	console.log(formattedAddress);
 	const path = `/json?address=${formattedAddress}`;
 	const response = await googleMapsConnector.get(path);
-	console.log(response);
+	if (response && response.results && response.results.length) {
+		const { lat, lng } = response.results[0] && response.results[0].geometry.location;
+		longitude = lat;
+		latitude = lng;
+	}
 
-
-	return response;
+	return {
+		longitude,
+		latitude
+	};
 }
 
 module.exports = {
